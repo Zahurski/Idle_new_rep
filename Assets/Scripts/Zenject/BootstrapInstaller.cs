f152c055146e5dc2c1855b981e64626d0cee42d1
Denis Gankov <22417706+sttrox@users.noreply.github.com>
2023-01-02 18:28:20 +0300

    Added BootstrapInstaller.cs

---
+++ Assets/Scripts/Zenject/BootstrapInstaller.cs
@@ -0,0 +1,24 @@
+﻿using IdleTycoon.Ads;
+using IdleTycoon.Ads.Implements;
+using IdleTycoon.Meta;
+using Zenject;
+
+namespace IdleTycoon.Zenject
+{
+    public class BootstrapInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.BindInterfacesTo<UnityRewardedAd>().AsSingle();
+            Container.BindInterfacesTo<UnityBannerAd>().AsSingle();
+            Container.BindInterfacesTo<UnityInterstitialAd>().AsSingle();
+
+            Container.Bind<IAdsInitializer>().To<UnityAdsInitializer>().AsSingle();
+
+            Container.Bind<MetaValues>().AsSingle();
+            Container.Bind<IMetaValues>().To<MetaValues>().FromResolve();
+
+            Container.BindInterfacesTo<Main>().AsSingle().NonLazy();
+        }
+    }
+}
