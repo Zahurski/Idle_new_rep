3a87ae16ea8ec25d4112d1136d9edff76222c900
Denis Gankov <22417706+sttrox@users.noreply.github.com>
2023-01-02 18:24:13 +0300

    Added GameScreen

---
+++ Assets/Scripts/UI/Views/HintMessageFly.cs
@@ -0,0 +1,61 @@
+﻿using AnimeTask;
+using Cysharp.Threading.Tasks;
+using IdleTycoon.Utils;
+using System;
+using System.Threading;
+using TMPro;
+using UnityEngine;
+using Easing = AnimeTask.Easing;
+
+namespace IdleTycoon.UI.Views
+{
+    public class HintMessageFly : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text label;
+        [SerializeField] private float startScale;
+        [SerializeField] private float speedFly;
+        [SerializeField] private float delayShowAfterMove;
+        [SerializeField] private float durationHide;
+        [SerializeField] private CanvasGroup canvasGroup;
+
+        private CancellationTokenSource animationCts;
+        private RectTransform startPoint;
+        private RectTransform endPoint;
+
+        public event Action<HintMessageFly> ShowEnded;
+
+        public void Reinitialize(RectTransform startPoint, RectTransform endPoint, string message)
+        {
+            this.endPoint = endPoint;
+            this.startPoint = startPoint;
+
+            label.text = message;
+        }
+
+        public void Show()
+        {
+            AsyncUtils.CancelAndCreateCts(ref animationCts, this.GetCancellationTokenOnDestroy());
+            RunAnimationAsync(animationCts.Token).Forget();
+        }
+
+        private async UniTask RunAnimationAsync(CancellationToken cancellationToken)
+        {
+            var distance = Vector2.Distance(startPoint.anchoredPosition, endPoint.anchoredPosition);
+
+            float durationFly = (distance / 100f) / speedFly;
+            await UniTask.WhenAll(
+                Easing.Create<Linear>(startPoint.anchoredPosition, endPoint.anchoredPosition, durationFly)
+                    .ToAnchoredPosition((RectTransform) this.transform, cancellationToken),
+                Easing.Create<Linear>(Vector3.one * startScale, Vector3.one, durationFly)
+                    .ToLocalScale(this, cancellationToken: cancellationToken),
+                Easing.Create<Linear>(0, 1, durationFly)
+                    .ToBind(() => canvasGroup.alpha, value => canvasGroup.alpha = value, cancellationToken: cancellationToken));
+
+            await UniTask.Delay(TimeSpan.FromSeconds(delayShowAfterMove), cancellationToken: cancellationToken);
+
+            await Easing.Create<Linear>(1, 0, durationHide)
+                .ToBind(() => canvasGroup.alpha, value => canvasGroup.alpha = value, cancellationToken: cancellationToken);
+            ShowEnded?.Invoke(this);
+        }
+    }
+}
