21a27348e8f5a529d02df868e80aa9e10f7f6559
Denis Gankov <22417706+sttrox@users.noreply.github.com>
2023-01-02 18:29:29 +0300

    Setup UIInstaller

---
+++ Assets/Scripts/Utils/AsyncUtils.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+
+namespace IdleTycoon.Utils
+{
+    public static class AsyncUtils
+    {
+        public static void CancelAndCreateCts(ref CancellationTokenSource cancellationTokenSource)
+        {
+            if (cancellationTokenSource != null)
+            {
+                cancellationTokenSource.Cancel();
+            }
+
+            cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        public static void CancelAndCreateCts(ref CancellationTokenSource cancellationTokenSource, CancellationToken linkedToken)
+        {
+            if (cancellationTokenSource != null)
+            {
+                cancellationTokenSource.Cancel();
+            }
+
+            cancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(linkedToken);
+        }
+    }
+}
