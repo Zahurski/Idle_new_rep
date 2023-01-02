805447e42783c64bc635bfbf7a67259ec50b5cb7
Denis Gankov <22417706+sttrox@users.noreply.github.com>
2023-01-02 14:21:58 +0300

    Registration ad config in DI

--- Assets/Scripts/Configs/MainSettingsConfigInstaller.cs
+++ Assets/Scripts/Configs/MainSettingsConfigInstaller.cs
@@ -1,20 +1,23 @@
-﻿using UnityEngine;
+﻿using IdleTycoon.Ads.Implements;
+using UnityEngine;
 using Zenject;
 
 namespace IdleTycoon.Configs
 {
-    [CreateAssetMenu(fileName = nameof(GameSettingsConfig), menuName = "Configs/" + nameof(GameSettingsConfig))]
-    public class GameSettingsConfig : ScriptableObjectInstaller<GameSettingsConfig>
+    [CreateAssetMenu(fileName = nameof(MainSettingsConfigInstaller), menuName = "Configs/" + nameof(MainSettingsConfigInstaller))]
+    public class MainSettingsConfigInstaller : ScriptableObjectInstaller<MainSettingsConfigInstaller>
     {
         [SerializeField] private GasStationConfig gasStationConfig;
         [SerializeField] private OilPumpConfig oilPumpConfig;
         [SerializeField] private EconomicConfig economicConfig;
+        [SerializeField] private AdsConfig adsConfig;
 
         public override void InstallBindings()
         {
             Container.BindInstance(gasStationConfig);
             Container.BindInstance(oilPumpConfig);
             Container.BindInstance(economicConfig);
+            Container.Bind<IUnityConfigAds>().FromInstance(adsConfig).AsSingle();
         }
     }
 }
