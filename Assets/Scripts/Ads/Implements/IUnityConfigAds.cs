ea721c6f9f2b51284e33bbc3fdcad10bb89e0d35
Denis Gankov <22417706+sttrox@users.noreply.github.com>
2023-01-02 10:31:14 +0300

    Added AdsConfig

---
+++ Assets/Scripts/Ads/Implements/IUnityConfigAds.cs
@@ -0,0 +1,17 @@
+﻿namespace IdleTycoon.Ads.Implements
+{
+    public interface IUnityConfigAds
+    {
+        bool IsDebugMode { get; }
+        bool IsTestMode { get; }
+        string AndroidBannerUnitId { get; }
+        string IOSBannerUnitId { get; }
+        AdsBannerPosition BannerPosition { get; }
+        string AndroidInterstitialUnitId { get; }
+        string IOSInterstitialUnitId { get; }
+        string AndroidRewardedUnitId { get; }
+        string IOSRewardedUnitId { get; }
+        string AndroidGameID { get; }
+        string IOSGameID { get; }
+    }
+}
