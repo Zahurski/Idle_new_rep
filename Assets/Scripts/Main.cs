00163dd926bead4b5b0e2ddb608e7a1ac7c10a49
Denis Gankov <22417706+sttrox@users.noreply.github.com>
2023-01-02 18:28:20 +0300

    Added BootstrapInstaller.cs

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,29 @@
+﻿using IdleTycoon.Ads;
+using IdleTycoon.Configs;
+using IdleTycoon.Meta;
+using Zenject;
+
+namespace IdleTycoon
+{
+    public class Main : IInitializable
+    {
+        private IAdsInitializer adsInitializer;
+        private MetaValues metaValues;
+        private EconomicConfig economicConfig;
+
+        [Inject]
+        private void Init(IAdsInitializer adsInitializer, MetaValues metaValues, EconomicConfig economicConfig)
+        {
+            this.economicConfig = economicConfig;
+            this.metaValues = metaValues;
+            this.adsInitializer = adsInitializer;
+        }
+
+        public void Initialize()
+        {
+            //d.gankov: here we can load saved data
+            adsInitializer.Initialize();
+            metaValues.SoftMoneyCoefficient = economicConfig.DefaultFactorSoftMoney;
+        }
+    }
+}
